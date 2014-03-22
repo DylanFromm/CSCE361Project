11aa58576cbf77bbe79e2d37f1051ee02860cbb2
DylanFromm <dylanfromm@hotmail.com>
2014-03-22 07:31:48 -0500

    Backup

---
+++ Assets/mButton.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mButton : MonoBehaviour {
+	
+	void OnGUI () {
+
+		
+		if (GUI.Button (new Rect (0, 0, 100, 100), "Quit")) {
+			Application.LoadLevel("MainMenu");
+		}
+	}
+}
